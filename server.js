9c2e548f0bfe8f1dccf1896b435538bbeb8a05d0
ola466@gmail.com <ola466@gmail.com>
2025-09-06 08:11:06 -0500

    first commit

--- server.js
+++ server.js
@@ -9,11 +9,28 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+
+/* ***********************
+ * View Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at view root
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
+
+
 
 /* ***********************
  * Local Server Information
@@ -28,3 +45,5 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
