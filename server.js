ba96a991f46ebfbb3c707f6e607b15874dcb74a9
ola466@gmail.com <ola466@gmail.com>
2025-09-27 11:18:52 -0500

    finished data validation

--- server.js
+++ server.js
@@ -12,7 +12,41 @@ const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
 const inventoryRoute= require("./routes/inventoryRoute")
-const utilities=require("./utilities/")
+const utilities = require("./utilities/")
+const session = require("express-session")
+const db = require("./database/")
+const accountRoute = require('./routes/accountRoute')
+const bodyParser = require("body-parser")
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require("connect-pg-simple")(session))({
+    createTableIfMissing: true,
+    pool: db.pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 
 /* ***********************
  * View Engine and Template
@@ -21,6 +55,7 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at view root
 app.use("/inv", inventoryRoute)
+app.use('/account', accountRoute)
 
 
 
@@ -63,10 +98,6 @@ app.use(async (err, req, res, next) => {
     nav
   });
 });
-
-
-
-
 
 
 
